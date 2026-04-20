c5d675ddd1d3df0cc77cbc2d13513e8c87d253f9
Thomas Steimann <thomas@steimann.ch>
2026-04-20 13:50:12 +0200

    fix: configure Sanity project id for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   volleyballApiBase: 'https://api.volleyball.ch/indoor',
   volleyballAuthToken: 'ef3775e2a47b975672144722db6ef8840258f873',
   volleyballClubId: 911080,
-  sanityProjectId: '',
+  sanityProjectId: 'u02qwa74',
   sanityDataset: 'production',
   sanityApiVersion: '2024-01-01',
 };
